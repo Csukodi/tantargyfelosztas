95e9ef3daea738a15f28692964b6e22523b7492d
csucsa <csukodicsaba12316@gmail.com>
2023-03-16 08:33:52 +0100

    7. feladat kesz

--- tanfel.py
+++ tanfel.py
@@ -90,6 +90,31 @@ be_osztaly=input("Osztály= ") or "10.b"
 be_tantargy=input("Tantárgy= ") or "kemia"
 print(f"Csoportbontásban tanulják")
 
+def csoportban_tanuljak(beo,be_o,be_t):
+    i=0
+    while i<len(beo) and not(beo[i]["osztaly"]==be_tantargy):
+        i+=1
+    return not(i<len(beo))
+
+
+if csoportban_tanuljak(beosztasok, be_osztaly, be_tantargy):
+    print("Csoportban tanuljak")
+else:
+    print("Osztalyszinten tanuljak")
+
+
+
 index=0
 while index<len(beosztasok) and not beosztasok[index]["osztaly"]==be_osztaly and beosztasok[index]["tantargy"]==be_tantargy:
     index+=1
+
+
+# 7. feladat
+#Az iskolában 49 tanár tanít.
+
+tanarok=[]
+for beosztas in beosztasok:
+    if beosztas["tanar"] not in tanarok:
+        tanarok.append(beosztas["tanar"])
+
+print(f"Az iskolában {len(tanarok)} tanár tanít.")
